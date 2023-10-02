692c12ff4a639e5e1961acd66e61e592c9a21baf
Fahad Kamal <fahadkamal@hotmail.com>
2023-10-02 08:51:59 -0400

    update query

--- 1_queries/property_listings_by_city.sql
+++ 1_queries/property_listings_by_city.sql
@@ -1,6 +1,8 @@
-SELECT properties.id, title, cost_per_night, avg(rating) FROM properties
-JOIN property_reviews ON properties.id = property_id
-WHERE rating >= 4 AND city = 'Vancouver'
+SELECT properties.id, title, cost_per_night, avg(property_reviews.rating) as average_rating
+FROM properties
+LEFT JOIN property_reviews ON properties.id = property_id
+WHERE city LIKE '%ancouv%'
 GROUP BY properties.id
+HAVING avg(property_reviews.rating) >= 4
 ORDER BY cost_per_night
 LIMIT 10;
